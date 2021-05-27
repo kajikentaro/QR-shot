d5ce57672fcb2b52d5c9ff42b6a2e1dc5722aa21
kajikentaro <k.kentarou0412@gmail.com>
2021-05-27 17:04:51 +0900

    add getting screenshot function and view full screen

--- QR shot/Form1.cs
+++ QR shot/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows;
 
 namespace QR_shot
 {
@@ -16,35 +17,10 @@ namespace QR_shot
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            /*
             Form2 form2 = new Form2();
             form2.Show();
-            */
-            capture();
-        }
-        private void maximize()
-        {
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.WindowState = FormWindowState.Maximized;
-        }
-        private void capture()
-        {
-            for(int i=0;i< Screen.AllScreens.Length; i++)
-            {
-                Rectangle rect = Screen.AllScreens[i].Bounds;
-                Bitmap img = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
-                Graphics g = Graphics.FromImage(img);
-                g.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size);
-                img.Save("test" + i + ".png", ImageFormat.Png);
-                System.Diagnostics.Process.Start(@".");
-
-                pictureBox1.ClientSize = new Size(rect.Size.Width,rect.Height);
-                pictureBox1.Image = (Image)img;
-            }
-
         }
     }
 }
--- QR shot/Form2.cs
+++ QR shot/Form2.cs
@@ -15,28 +15,56 @@ namespace QR_shot
         public Form2()
         {
             InitializeComponent();
+            capture();
+            maximize();
         }
 
         private void maximize()
         {
             this.FormBorderStyle = FormBorderStyle.None;
-            this.WindowState = FormWindowState.Maximized;
+            this.StartPosition = FormStartPosition.Manual;
+            //this.WindowState = FormWindowState.Maximized;
+
+            WindowStateHolder wsh = new WindowStateHolder();
+            this.Location = new Point(wsh.sourceX, wsh.sourceY);
+            this.Size = wsh.size;
         }
         private void capture()
         {
+            WindowStateHolder wsh = new WindowStateHolder();
+            Bitmap img = new Bitmap(wsh.width, wsh.height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(img);
+            g.CopyFromScreen(wsh.sourceX, wsh.sourceY, 0, 0, wsh.size);
+            img.Save("test.png", ImageFormat.Png);
+            System.Diagnostics.Process.Start(@".");
+
+            pictureBox1.ClientSize = wsh.size;
+            pictureBox1.Image = (Image)img;
+
+        }
+    }
+    public class WindowStateHolder
+    {
+        const int inf = 100100100;
+        public int sourceX { get; } = inf;
+        public int sourceY { get; } = inf;
+        public int destinationX { get; } = -inf;
+        public int destinationY { get; } = -inf;
+        public int height { get; }
+        public int width{ get; }
+        public Size size { get; }
+        public WindowStateHolder(){
             for(int i=0;i< Screen.AllScreens.Length; i++)
             {
                 Rectangle rect = Screen.AllScreens[i].Bounds;
-                Bitmap img = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
-                Graphics g = Graphics.FromImage(img);
-                g.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size);
-                img.Save("test" + i + ".png", ImageFormat.Png);
-                System.Diagnostics.Process.Start(@".");
-
-                pictureBox1.ClientSize = new Size(rect.Size.Width,rect.Height);
-                pictureBox1.Image = (Image)img;
+                sourceX = Math.Min(sourceX, rect.X);
+                sourceY = Math.Min(sourceY, rect.Y);
+                destinationX = Math.Max(destinationX, rect.X + rect.Width);
+                destinationY = Math.Max(destinationY, rect.Y + rect.Height);
             }
-
+            width = destinationX - sourceX;
+            height = destinationY - sourceY;
+            size = new Size(width, height);
         }
     }
 }
